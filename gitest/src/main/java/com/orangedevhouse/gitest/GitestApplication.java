8d229e5f42cf6df25b6121a805ffaff546143747
OrangeTestHouse <allissonjardel@gmail.com>
2025-06-19 17:07:58 -0300

    Testando .gitignore

---
+++ gitest/src/main/java/com/orangedevhouse/gitest/GitestApplication.java
@@ -0,0 +1,13 @@
+package com.orangedevhouse.gitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitestApplication.class, args);
+	}
+
+}
